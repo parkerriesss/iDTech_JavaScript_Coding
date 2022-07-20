4ad6991501bce51c03f7785e4e7dba36e50dae4f
parkerriesss <109682929+parkerriesss@users.noreply.github.com>
2022-07-20 15:07:32 -0400

    Add files via upload

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,37 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+
+let welcome = "Hello there young traveler!"
+let name = "Harrold"
+let place = "tavern"
+let guild = "Blue Dragons" 
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+
+let cost = 500
+let numberOfPotions = 10
+let numberOfBandages = 5
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+let listOfItems = ["Root Beer", "Pancake", "Bandages", "Twine"]
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+let goodPotion = true
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+
+console.log(welcome + " This is my ol' " + place + ".");
+console.log("My name is " + name + " i'm the owner here.")
+console.log("I come from the "+ guild + " guild, im looking to buy some items.")
+console.log("That sounds great! What would you be interested in?")
+console.log("I want " + numberOfPotions + " potions, I think the healing potion would be best.")
+console.log(goodPotion + ", the healing potion is a great choice!")
+console.log("Im also looking to buy " + numberOfBandages + " " + listOfItems[2] + ".")
+console.log("Great! that'll cost " + cost + " coins.")
+console.log("Goodluck on your future travels adventurer! Hope to see you soon!")
